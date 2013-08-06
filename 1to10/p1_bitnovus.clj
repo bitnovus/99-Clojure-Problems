acbffa40c89d9d0252186784234e3f8c8b8bfa1e
Daniel Chyan <dchyan1@gmail.com>
2013-08-06 01:11:14 -0400

    Rewrote p1 to not use 'last'

--- 1to10/p1_bitnovus.clj
+++ 1to10/p1_bitnovus.clj
@@ -1,7 +1,10 @@
 ;;; Find the last box of a list.
 
 (defn my-last [input] 
-    (last input))
+    (loop [cur-list input result nil]
+        (if (empty? cur-list)
+            result
+            (recur (pop cur-list) (peek cur-list)))))
 
 (defn -main [& args]
     (println (my-last '(1 2 3 4))))
